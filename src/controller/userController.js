50b70a800ef56485506dec852186e9086d6914be
Chakrit <chakritkp@hotmail.com>
2024-05-29 17:44:25 +0700

    change hash password

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,7 @@
 import bcrypt from "bcrypt";
+import CryptoJS from "crypto-js";
+import crypto from "crypto";
+import { Buffer } from "node:buffer";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 import { Users } from "../models/usersModel.js";
@@ -7,6 +10,39 @@ import { Role } from "../models/roleModel.js";
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
+const saltRounds = parseInt(process.env.SALT);
+
+const KEY = process.env.RANDOMBYTES;
+const SECRET_HEX = process.env.AES_SECRET_HEX;
+
+const secretKeyBuffer = KEY;
+const secretKey = secretKeyBuffer.toString(SECRET_HEX);
+
+// let en = CryptoJS.AES.encrypt("123465", secretKey).toString();
+// console.log(en);
+// let de = CryptoJS.AES.decrypt(en, secretKey).toString(CryptoJS.enc.Utf8);
+// console.log(de);
+
+const encrypt = (password, message, callback) => {
+  try {
+    const encryPassword = CryptoJS.AES.encrypt(password, message).toString();
+    callback(null, encryPassword);
+  } catch (error) {
+    callback(error);
+  }
+};
+
+const decrypted = (password, message, callback) => {
+  try {
+    const originalMessage = CryptoJS.AES.decrypt(password, message).toString(
+      CryptoJS.enc.Utf8
+    );
+
+    callback(null, originalMessage);
+  } catch (error) {
+    callback(error);
+  }
+};
 
 export const getUsers = async (req, res) => {
   try {
@@ -71,8 +107,6 @@ export const registerUser = async (req, res) => {
     };
     let result = {};
 
-    const saltRounds = parseInt(process.env.SALT);
-
     if (email && phone_number && password) {
       const validateUser = await Users.findOne({
         $or: [{ email: email }, { phone_number: phone_number }],
@@ -85,26 +119,50 @@ export const registerUser = async (req, res) => {
           .json({ message: "E-mail or phone number is already." });
       } else {
         if (password) {
-          bcrypt.genSalt(saltRounds, (err, salt) => {
+          encrypt(password, secretKey, (err, encryPassword) => {
             if (err) {
-              return;
-            }
-            bcrypt.hash(password, salt, (err, hash) => {
-              if (err) {
-                return;
-              }
+              console.error(err);
+            } else {
               result = new Users({
                 email: email,
                 phone_number: phone_number,
-                password: hash,
+                password: encryPassword,
                 ...timeStamp,
               });
               result.save();
               return res
                 .status(200)
                 .json({ message: "Create user successed." });
-            });
+
+              // decrypted(encryPassword, secretKey, (err, originalMessage) => {
+              //   if (err) {
+              //     console.error(err);
+              //   } else {
+              //     console.log(originalMessage === password);
+              //   }
+              // });
+            }
           });
+          // bcrypt.genSalt(saltRounds, (err, salt) => {
+          //   if (err) {
+          //     return;
+          //   }
+          //   bcrypt.hash(password, salt, (err, hash) => {
+          //     if (err) {
+          //       return;
+          //     }
+          //     result = new Users({
+          //       email: email,
+          //       phone_number: phone_number,
+          //       password: hash,
+          //       ...timeStamp,
+          //     });
+          //     result.save();
+          //     return res
+          //       .status(200)
+          //       .json({ message: "Create user successed." });
+          //   });
+          // });
         }
       }
     } else {
@@ -148,27 +206,52 @@ export const loginUser = async (req, res) => {
     }
 
     if (user._id && user.email && user.password && user.is_active) {
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        return res.status(400).json({
-          message: "Invalid username or password",
-        });
-      }
-
-      const token = jwt.sign(
-        {
-          id: user._id,
-          email: user.email,
-          role_id: user.role_id,
-        },
-        JWT_SECRET,
-        { expiresIn: 60 * 60 * 24 * 7 }
-      );
-
-      return res.status(200).json({
-        message: "Login Successful",
-        token,
+      decrypted(user.password, secretKey, (err, originalMessage) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const isMatch = originalMessage === password;
+          if (!isMatch) {
+            return res.status(400).json({
+              message: "Invalid username or password",
+            });
+          }
+          const token = jwt.sign(
+            {
+              id: user._id,
+              email: user.email,
+              role_id: user.role_id,
+            },
+            JWT_SECRET,
+            { expiresIn: 60 * 60 * 24 * 7 }
+          );
+          return res.status(200).json({
+            message: "Login Successful",
+            token,
+          });
+        }
       });
+      // const isMatch = decrypted(secretKey, password);
+      // console.log(isMatch);
+      // const isMatch = await bcrypt.compare(password, user.password);
+      // if (!isMatch) {
+      //   return res.status(400).json({
+      //     message: "Invalid username or password",
+      //   });
+      // }
+      // const token = jwt.sign(
+      //   {
+      //     id: user._id,
+      //     email: user.email,
+      //     role_id: user.role_id,
+      //   },
+      //   JWT_SECRET,
+      //   { expiresIn: 60 * 60 * 24 * 7 }
+      // );
+      // return res.status(200).json({
+      //   message: "Login Successful",
+      //   token,
+      // });
     }
   } catch (error) {
     console.error(error);
