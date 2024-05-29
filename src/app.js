41de8d8b3ca11da2ed891c96006926e7bfc57f81
Chakrit <chakritkp@hotmail.com>
2024-05-30 01:32:29 +0700

    add domain

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express, { json } from "express";
-import cors from 'cors'
+import cors from "cors";
 import router from "./routers/router.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -13,22 +13,29 @@ const DBURI = process.env.DATABASENAME;
 const DATAAUTH = process.env.DATABASEAUTH;
 
 app.use(json());
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://testing-chakrit.web.app'],
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://testing-chakrit.web.app",
+      "https://testing-chakrit.firebaseapp.com",
+    ],
     credentials: true,
-}))
+  })
+);
 
-mongoose.connect(`mongodb+srv://${DBURI}:${DATAAUTH}.yf01htl.mongodb.net/${DB}`);
+mongoose.connect(
+  `mongodb+srv://${DBURI}:${DATAAUTH}.yf01htl.mongodb.net/${DB}`
+);
 const db = mongoose.connection;
 
-db.on('error', (error) => {
-  console.error('MongoDB connection error:', error);
+db.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
 });
 
-db.once('open', () => {
-  console.log('Connected to MongoDB successfully');
+db.once("open", () => {
+  console.log("Connected to MongoDB successfully");
 });
-
 
 app.use("/api-services", router);
 app.listen(port, () => {
